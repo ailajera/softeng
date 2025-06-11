de01a6c2ed802903e46b639d4f79f03b3805c3aa
ailajera <jeraila.evangelista@gmail.com>
2025-06-12 00:12:12 +1200

    activities for string

---
+++ python_refresher/strings.py
@@ -0,0 +1,25 @@
+#mga activity ni bry
+
+word = "Python"
+
+print(word[:3], "\n")
+print(word[-3:], "\n")
+print(word[1:4], "\n")
+
+word2 = "hello"
+
+print(word2[::-1])
+
+name = "bryan"
+
+print(name[0])
+print(name[-1])
+
+greeting = "Hi There"
+
+print(greeting.lower())
+print(greeting.upper())
+
+word1 = f"{word2}! {greeting} {name}!"
+
+print(word1)
