6e71b50229357ae13e57d932aed2842df0510729
ailajera <jeraila.evangelista@gmail.com>
2025-06-27 01:48:10 +1200

    ewan ang mema naaral ko til diff ng operation at first ng loops

---
+++ python_refresher/study1/basic_1stpart.py
@@ -0,0 +1,11 @@
+#babaguhin ko pa to mema in lang para alam kong na gets ko
+
+#INPUT 
+
+wholename =  input("please enter your name: ")
+age = input("enter your age: ")
+yearofschool = input("what is your year level? ")
+
+print("Hello there," + wholename + "\n in the age of " + age + "\n your year level is \n" + yearofschool)
+
+# si operation naaral ko na to be fair. pero di klo na gagawan sakit ulo ko talaga
