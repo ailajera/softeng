a13c30869cea43e5dff761e780b16433b665ca14
ailajera <jeraila.evangelista@gmail.com>
2025-07-01 21:34:46 +1200

    di pa tapos pero up ko na login

--- activities/login_system.py
+++ activities/login_system.py
@@ -1,7 +1,12 @@
 # START HERE
 def login(username, password):
-    pass
-
+    
+    if username == "admin" and password == "1234":
+        print("Login Successful")
+    else:
+        print("Invalid credentials")
+# napipikon na ko kanina pa ko nagbabago. di ko alam bat failed bye. 
+#antok na ko bukas na to
 ##
 
 if __name__ == "main":
