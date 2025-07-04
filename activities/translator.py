b9f2d8d710ccd471e4a46b8efd007460d678d7f1
ailajera <jeraila.evangelista@gmail.com>
2025-07-04 23:40:36 +1200

    translation of words

--- activities/translator.py
+++ activities/translator.py
@@ -1,7 +1,15 @@
 # START HERE
 def translator():
-    pass
+    translate = {
+        "hello" : "kamusta",
+        "thank you" : "salamat",
+        "goodbye" : "paalam"
+    }
 
+    phrase = input().lower() 
+
+    translates = translate.get(phrase,"Translation not found")
+    print(f"Filipino: {translates}")
 ##
 
 if __name__ == "__main__":
