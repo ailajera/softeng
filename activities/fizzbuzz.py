35e4d4e7f7dbfa4604da643d0b5fcaa3aa4ad0d2
Bryan Tabares <tabaresbryan39@gmail.com>
2025-07-13 20:05:01 +0800

    sagot ni aya

--- activities/fizzbuzz.py
+++ activities/fizzbuzz.py
@@ -1,16 +1,17 @@
 def fizzbuzz(n):
-    """
-    Given an integer n, return a list of strings representing numbers from 1 to n.
-    For multiples of 3, add "Fizz" instead of the number.
-    For multiples of 5, add "Buzz" instead of the number.
-    For multiples of both 3 and 5, add "FizzBuzz".
-    """
-    #
-    pass
-
+    result = []
+    for i in range(1, n+1):
+        if i%3 == 0 and i%5 == 0:
+            result.append("FizzBuzz")
+        elif i%3 == 0:
+            result.append("Fizz")
+        elif i%5 == 0:
+            result.append("Buzz")
+        else:
+            result.append(str(i))
+    return result
 
 if __name__ == "__main__":
     number = int(input("Enter a number: "))
     result = fizzbuzz(number)
-    for item in result:
-        print(item)
+    print(result)
