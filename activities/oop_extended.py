e6dffaa6d9245a0b87a491510872827c010b9521
ailajera <jeraila.evangelista@gmail.com>
2025-08-12 23:56:04 +1200

    extended version of the oop

--- activities/oop_extended.py
+++ activities/oop_extended.py
@@ -10,16 +10,85 @@ class Pet:
     # create the required methods here
 
     def eat(self):
-        self.energy += 10
-        print(self.name, "already ate and has energy: ", self.energy)
+        
+        if self.energy >= 100:
+            print(self.name, "doesn't like to eat.", self.energy)
+        else: 
+            gained_energy = 10
+            self.energy += gained_energy 
+            print(self.name, "already ate and has energy: ", self.energy)
 
     def sleep(self):
-        self.energy += 25
-        print(self.name, "slept and has an energy: ", self.energy)
+
+        if self.energy >= 100:
+            print(self.name, "doesn't want to sleep", self.energy)
+        else:
+            gained_energy = 25
+            self.energy += gained_energy
+            print(self.name, "slept and has an energy: ", self.energy)
 
     def play(self):
-        self.energy -= 15
-        print(self.name, "played and now tired with energy: ", self.energy)
+        
+        if self.energy < 20:
+            print(self.name, "low energy, can't play anymore", self.energy)
+        else: 
+            lost_energy = 15
+            self.energy -= lost_energy 
+            print(self.name, "played and now tired with energy: ", self.energy)
 
     def show_status(self):
         print(f"{self.name} the {self.pet_type} has {self.energy} energy.")
+
+#DOG
+
+class Dog(Pet):
+    def __init__(self, name):
+        super().__init__(name,pet_type) 
+        pet_type = "dog"
+
+    
+    def sleep(self):
+
+        if self.energy >= 100:
+            print(f"{self.name } doesn't want to sleep")
+        else:
+            gained_energy = 25
+            self.energy += gained_energy
+            print(self.name, "slept and has an energy: ")
+
+#CAT
+
+class Cat(Pet):
+    def __init__(self, name):
+        super().__init__(name, pet_type)
+        pet_type = "cat"
+
+    def play(self):
+        
+        if self.energy < 20:
+            print(f"{self.name}low energy, can't play anymore")
+        else: 
+            lost_energy = 15
+            self.energy -= lost_energy 
+            print(f"{self.name}played and now bored. It has {lost_energy} energy and the energy {self.energy} now")
+
+#BIRD
+
+
+class Bird(Pet):
+    def __init__(self, name):
+        super().__init__(name, pet_type)
+        pet_type = "bird"
+
+    def eat(self):
+        
+        if self.energy >= 100:
+            print(f"{self.name} still full and doesn't like to eat.")
+        else: 
+            gained_energy = 10
+            self.energy += gained_energy
+            print(f"{self.name} already ate and has energy: ")    
+
+    
+
+    
