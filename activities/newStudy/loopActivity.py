44ff2fa518768e3d79075a24d834068a3c37be0e
ailajera <jeraila.evangelista@gmail.com>
2026-05-05 22:09:55 +1200

    for loop

---
+++ activities/newStudy/loopActivity.py
@@ -0,0 +1,16 @@
+"""
+Exercise 1: Countdown
+
+Instructions:
+Using for loop
+Ask the user for a number
+Count down to 0
+Print each number
+"""
+#example of for loop
+
+ans = int(input("Gimme Number: "))
+
+for i in range(ans):
+    print(i)
+    
